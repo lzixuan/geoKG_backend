1faf5d0dbfd72e80283c9f63aebe38c5d2ab48e4
lzixuan <395862131@qq.com>
2019-05-04 22:51:27 +0800

    add overviewbytopic

--- interface.py
+++ interface.py
@@ -36,7 +36,7 @@ class Place:
         return (self.score < other.score)
     def toDict(self):
         temp = {}
-        temp['index'] = self.index
+        temp['key'] = self.index
         temp['name'] = self.name
         temp['qu'] = self.qu
         temp['topic'] = self.topic
@@ -54,13 +54,15 @@ def search():
     means = request.form.get('means')
     if means == "any":
         means = ''
+    else:
+        means = ':' + means
     place = request.form.get('place')
     transfer = request.form.get('transfer')
     time = request.form.get('time')
     topics = literal_eval(request.form.get('topics'))
     print(means, place, transfer, time, topics)
     #get all the candidates
-    cypher = 'match (p1:Place{name:"%s"})-[:%s*..%s]-(p2:Place) return distinct p2' % (place, means, transfer)
+    cypher = 'match (p1:Place{name:"%s"})-[%s*..%s]-(p2:Place) return distinct p2' % (place, means, transfer)
     result = graph.run(cypher).data()
     #print(result[0]['p2'])
     nodes = []
@@ -73,21 +75,13 @@ def search():
                         yield nodeId, cost 
                         return algo.getNodeById(nodeId).name as name, cost''' % (place, place2, means)
             result_time = graph.run(cypher2).data()
-            minTime = result_time[len(result_time) - 1]['cost']
         else:
             cypher2 = '''match (start:Place{name:"%s"}),(end:Place{name:"%s"})
-                        call algo.shortestPath.stream(start,end,"time",{relationshipQuery:"bus",direction:"both"}) 
+                        call algo.shortestPath.stream(start,end,"time",{relationshipQuery:null,direction:"both"}) 
                         yield nodeId, cost 
                         return algo.getNodeById(nodeId).name as name, cost''' % (place, place2)
             result_time = graph.run(cypher2).data()
-            minTime1 = result_time[len(result_time) - 1]['cost']
-            cypher2 = '''match (start:Place{name:"%s"}),(end:Place{name:"%s"})
-                        call algo.shortestPath.stream(start,end,"time",{relationshipQuery:"subway",direction:"both"}) 
-                        yield nodeId, cost 
-                        return algo.getNodeById(nodeId).name as name, cost''' % (place, place2)
-            result_time = graph.run(cypher2).data()
-            minTime2 = result_time[len(result_time) - 1]['cost']
-            minTime = min(minTime1, minTime2)
+        minTime = result_time[len(result_time) - 1]['cost']
         if minTime <= int(time):
             nodes.append(node)
             minTimes.append(minTime)
@@ -114,6 +108,31 @@ def search():
     for i in range(0, len(places)):
         temp = places[i].toDict()
         temp['rank'] = i + 1
+        places_response.append(temp)
+    return Response(json.dumps({"places": places_response}), mimetype="application/json")
+
+@app.route('/coordinate', methods=['POST'])
+def coordinate():
+    place = request.form.get('place')
+    cypher = 'match (p:Place{name:"%s"}) return p.lat, p.lng' % place
+    result = graph.run(cypher).data()
+    coord = {'lat':result[0]['p.lat'], 'lng':result[0]['p.lng']}
+    return Response(json.dumps(coord), mimetype="application/json")
+
+@app.route('/viewbytopic', methods=['POST'])
+def viewbytopic():
+    topic = request.form.get('topic')
+    print(topic)
+    cypher = 'match (p:Place) return p'
+    nodes  = graph.run(cypher).data()
+    places_response = []
+    for i in range(0, len(nodes)):
+        temp = {}
+        temp['key'] = i
+        temp['name'] = nodes[i]['p']['name']
+        temp['qu'] = nodes[i]['p']['qu']
+        temp['topic'] = round(10000 * nodes[i]['p']['topic'+topic_num[topic]], 2)
+        temp['posRate'] = round(100 * nodes[i]['p']['posRate'+topic_num[topic]], 2)
         places_response.append(temp)
     return Response(json.dumps({"places": places_response}), mimetype="application/json")
 
